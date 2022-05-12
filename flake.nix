8c16915e490f41a3ba5b8adf47999084f49040f4
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:28 -0300

    chore: re index circleci_client

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."circleci_client-master".dir   = "master";
+  inputs."circleci_client-master".owner = "nim-nix-pkgs";
+  inputs."circleci_client-master".ref   = "master";
+  inputs."circleci_client-master".repo  = "circleci_client";
+  inputs."circleci_client-master".type  = "github";
+  inputs."circleci_client-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."circleci_client-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
